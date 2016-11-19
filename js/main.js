a835c1d6ba707e2254a0ddde73a71fb254933a7f
Lucas Soares <lucas.soares.araujo@hotmail.com>
2016-11-19 14:46:50 -0300

    melhorias no código js e html

--- js/main.js
+++ js/main.js
@@ -6,40 +6,81 @@ document.addEventListener('DOMContentLoaded', function() {
         this.contador = 0;     
     }
 
-    Gato.prototype.incrementar = function() {
+Gato.prototype.incrementar = function() {
         this.contador += 1;
     }
 
-    var modal = {
+    var model = {
+        gatoAtual: null,
         gatos: [new Gato("Teste", "img/img.jpg"), new Gato("Gato Selvagem", "img/selvagem.jpg"), new Gato("Miau", "img/gato.jpg"), new Gato("Teste2", "img/assustado.jpg"), new Gato("Miau", "img/img.jpg")] 
     };
 
     var controller = {
         adicionarGato: function (gato) {
-            modal.gatos.push(gato);
+            model.gatos.push(gato);
             view.listarGatos();
             view.limparTelaCadastro();
         },
 
+        incrementarContador: function(){
+            model.gatoAtual.incrementar();
+            gatoView.renderizarGato();
+        },
+
         pegarGatos: function(){
-            return modal.gatos;
+            return model.gatos;
+        },
+
+        pegarGatoAtual: function(){
+            return model.gatoAtual;
+        },
+
+        setGatoAtual: function(gato){
+            model.gatoAtual = gato;
         },
 
         init: function() {
+            model.gatoAtual = model.gatos[0];
             view.init();
+            gatoView.init();
+            
         }
 
 
     };
 
+    var gatoView = {
+        init: function(){
+            this.divGato = document.getElementById('cat');
+            this.imgGato = document.getElementById('cat-img');
+            this.textoGato = document.getElementById('cat-counter');
+
+            this.imgGato.addEventListener('click', function(){
+                controller.incrementarContador();
+            });
+
+            this.renderizarGato();
+        },
+
+        renderizarGato: function(){
+            var gatoAtual = controller.pegarGatoAtual();
+            this.imgGato.src = gatoAtual.foto;
+            this.textoGato.innerText = `Numero de cliques no gato ${gatoAtual.nome}: ${gatoAtual.contador}`;
+        }
+    
+    }
+
     var view = {
 
         init: function() {
-            var addFotoBtn = document.getElementById('cat-img');
+            var addFotoBtn = document.getElementById('new-cat-img');
             addFotoBtn.addEventListener('change', view.pegarFoto, false);
 
             var addGatoBtn = document.getElementById('novo-gato-btn');
             addGatoBtn.addEventListener('click',function(e){
+
+                
+
                 if(view.novaImg){
 
                     var nomeGato = document.getElementById('nome').value;
@@ -63,44 +104,45 @@ document.addEventListener('DOMContentLoaded', function() {
                 
                 lista.appendChild(liGato);
 
-                liGato.addEventListener('click', function(){              
-                    view.mostrarGato(gato);                
+                liGato.addEventListener('click', function(){
+                    controller.setGatoAtual(gato);              
+                    gatoView.renderizarGato();                
                 }, false);
             });
         },
 
-        mostrarGato: function(gato){
-            var conteudo = document.getElementById('conteudo');
-            conteudo.innerHTML = '';
+        // mostrarGato: function(gato){
+        //     var conteudo = document.getElementById('conteudo');
+        //     conteudo.innerHTML = '';
 
-            var divGato = document.createElement('div');
-            var imgGato = document.createElement('img');
-            var textoGato = document.createElement('p');
-            var contadorGato = document.createElement('span');
+        //     var divGato = document.createElement('div');
+        //     var imgGato = document.createElement('img');
+        //     var textoGato = document.createElement('p');
+        //     var contadorGato = document.createElement('span');
 
-            divGato.setAttribute('class', 'gato');
+        //     divGato.setAttribute('class', 'gato');
         
-            imgGato.setAttribute('src', gato.foto);
+        //     imgGato.setAttribute('src', gato.foto);
             
 
-            textoGato.innerText = "Numero de cliques no gato "+gato.nome+ ": ";
-            contadorGato.innerText = gato.contador;
+        //     textoGato.innerText = "Numero de cliques no gato "+gato.nome+ ": ";
+        //     contadorGato.innerText = gato.contador;
 
 
-            divGato.appendChild(imgGato);
-            textoGato.appendChild(contadorGato);
-            divGato.appendChild(textoGato);
-            conteudo.appendChild(divGato);
+        //     divGato.appendChild(imgGato);
+        //     textoGato.appendChild(contadorGato);
+        //     divGato.appendChild(textoGato);
+        //     conteudo.appendChild(divGato);
 
-            imgGato.addEventListener('click', function () {
-                gato.incrementar();
-                contadorGato.innerText = gato.contador;
-            }, false);
-        },
+        //     imgGato.addEventListener('click', function () {
+        //         gato.incrementar();
+        //         contadorGato.innerText = gato.contador;
+        //     }, false);
+        // },
 
         pegarFoto: function (e) {
             var arquivo = e.target.files[0];
-            console.log(arquivo.type);
+            
             var filtro = RegExp('image/*');
             if(filtro.test(arquivo.type)){
                 var imagem = new FileReader();
